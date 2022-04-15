16dc68f19efbe00ad501bf1ae638bd98ee29e43a
Sahib Muradov <32236360+sahibziko@users.noreply.github.com>
2022-04-15 04:02:44 +0400

    Create yazdirsil.py

---
+++ userbot/modules/yazdirsil.py
@@ -0,0 +1,17 @@
+import random
+from telethon import events
+from userbot.events import register
+from userbot.cmdhelp import CmdHelp
+
+@register(outgoing=True, pattern="^.yazdir ?(.*)")
+async def _(event):
+    arzu = event.pattern_match.group(1)
+    faiz = random.randint(0, 100)
+    if not arzu:
+       await event.edit("` nə yazdırım qaqa :)`")
+    if arzu:
+       await event.edit(f"**icra olundu!**")
+       
+Help = CmdHelp('arzu')
+Help.add_command('arzu', '<arzunuz>', 'Arzunuzun reallaşmaq ehtimalını öyrənin', 'arzu Universitetə qəbul olmaq')
+Help.add_info('@UseratorOT `üçün hazırlanıb`').add()
