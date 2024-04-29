2a1ff148653cf38c77cbc60aa5f7cd9fa74c6905
Sahib Muradov <32236360+sahibziko@users.noreply.github.com>
2024-04-29 17:25:31 +0400

    .apex

--- userbot/modules/sudo.py
+++ userbot/modules/sudo.py
@@ -7,7 +7,7 @@ from userbot.events import register
 
 @register(incoming=True, from_users=SUDO_ID, pattern="^.salive$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`U S Σ R Δ T O R ＳＵＤＯ ✨`")
+    await q.client.send_message(q.chat_id,"`𝙰 𝙿 Σ 𝚇 - ＳＵＤＯ ✨`")
 
 CmdHelp('sudo').add_command(
     'salive', None, 'SUDOnun aktiv olub olmadığını yoxlayar.'
