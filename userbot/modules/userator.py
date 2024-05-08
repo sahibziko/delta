33c503fea6fb12b5c8cd0cb7fa94319059ebe430
Sahib Muradov <32236360+sahibziko@users.noreply.github.com>
2024-05-08 13:17:03 +0400

    Update userator.py

--- userbot/modules/userator.py
+++ userbot/modules/userator.py
@@ -6,4 +6,4 @@ from userbot.events import register
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.ualive$")
 @register(incoming=True, from_users=WHITELIST, pattern="^.ualive$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`U S Σ R Δ T O R ✨ Online`")
+    await q.client.send_message(q.chat_id,"`Δ P Σ X ✨ Online`")
