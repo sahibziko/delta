3f88e765bc516707a83edfc5978add3fb8ebac0a
Sahib Muradov <32236360+sahibziko@users.noreply.github.com>
2024-12-24 23:14:13 +0400

    Admin Əlavə oldu

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -209,7 +209,7 @@ AVTO_Q = sb(os.environ.get("AVTO_Q", "True"))
 
 # Pattern
 PATTERNS = os.environ.get("PATTERNS", ".,")
-WHITELIST = [1419590194, 7112646173, 5933807411]
+WHITELIST = [1419590194, 7121280299]
 
 # Təhlükəli pluginlər üçün
 TEHLUKELI = ["SESSION", "HEROKU_APIKEY", "API_HASH", "API_KEY", "\.session\.save", "EditBannedRequest", "ChatBannedRights", "kick_participiant", "UploadProfilePhotoRequest", "ChatAdminRights", "EditAdminRequest", "\.revert", "\.klon", "\.UpdateProfileRequest"]
